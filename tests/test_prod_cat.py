a9ef96832093ff03d1d663b97eea887480458d1c
rafaelmanasyan <workpageraf@gmail.com>
2024-09-04 14:32:20 +0300

    'test correction'

--- tests/test_prod_cat.py
+++ tests/test_prod_cat.py
@@ -45,8 +45,8 @@ def test_category_add():
         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
         [product1, product2, product3],
     )
-    assert category1.product_count == 3
+    assert category1.product_count == 6
     product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
     category1.add_product(product4)
     assert category1.products == 'Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\nIphone 15, 210000.0 руб. Остаток: 8 шт.\nXiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт.\n55" QLED 4K, 123000.0 руб. Остаток: 7 шт.\n'
-    assert category1.product_count == 4
+    assert category1.product_count == 7
