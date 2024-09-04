a28b39a92aa7fdd1da6d90f6a1e451f662cb5585
rafaelmanasyan <workpageraf@gmail.com>
2024-09-04 14:30:55 +0300

    'write tests'

--- tests/test_prod_cat.py
+++ tests/test_prod_cat.py
@@ -1,3 +1,6 @@
+from src.product_category import *
+
+
 def test_init_product(product_iphone):
     assert product_iphone.name == "Iphone 15"
     assert product_iphone.description == "512GB, Gray space"
@@ -7,3 +10,43 @@ def test_init_cat(category_smartphones):
     assert category_smartphones.name == "Смартфоны"
     assert category_smartphones.category_count == 1
     assert category_smartphones.product_count == 3
+
+
+def test_new_product():
+    new_product = Product.new_product({
+        "name": "Samsung Galaxy S23 Ultra",
+        "description": "256GB, Серый цвет, 200MP камера",
+        "price": 180000.0,
+        "quantity": 5,
+    })
+    assert new_product.description == "256GB, Серый цвет, 200MP камера"
+
+
+def test_price_setter():
+    new_product = Product.new_product({
+        "name": "Samsung Galaxy S23 Ultra",
+        "description": "256GB, Серый цвет, 200MP камера",
+        "price": 180000.0,
+        "quantity": 5,
+    })
+    new_product.price = -100000.0
+    assert new_product.price == 180000.0
+
+
+def test_category_add():
+    product1 = Product(
+        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
+    )
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3],
+    )
+    assert category1.product_count == 3
+    product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+    category1.add_product(product4)
+    assert category1.products == 'Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.\nIphone 15, 210000.0 руб. Остаток: 8 шт.\nXiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт.\n55" QLED 4K, 123000.0 руб. Остаток: 7 шт.\n'
+    assert category1.product_count == 4
